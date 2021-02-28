a5d0ffb74a1a326d4f7c19e5c98a97f0fe24e578
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-02-27 21:49:04 -0800

    Use regex to determine backseater messages

--- bot.js
+++ bot.js
@@ -21,20 +21,43 @@ client.on('connected', onConnectedHandler);
 
 client.connect();
 
-const pieceNamePattern = /\bpawn\b|\bknight\b|\bbishop\b|\brook\b|\bking\b|\bqueen\b/gmi;
-const backseatKeywordsPattern = /takes|mate|missed|checkmate|defend|with/gmi;
-const chessNotationPattern = /\b[nbkqr]?x?[a-h]{1}[1-8]{1}\b/gmi;
+const pieceNames = /\bpawn\b|\bknight\b|\bbishop\b|\brook\b|\bking\b|\bqueen\b/gmi;
+const backseatKeywords = /takes|take|mate|missed|checkmate|defend|with|check/gmi;
+const chessNotation = /\b[nbkqr]?x?[a-h]{1}[1-8]{1}\b/gmi;
+
+function timeoutUser(target, user) {
+    client.say(target, `/timeout ${user} 10`);
+    console.log(`* Timing out ${user}`);
+}
+
+function backseating(backseatMatches, pieceMatches, chessMoveMatches) {
+    if (backseatMatches)
+        if (backseatMatches.length >= 2) return true;
+    if (chessMoveMatches)
+        if (chessMoveMatches.length >= 1) return true;
+    if (backseatMatches && pieceMatches)
+        if (backseatMatches.length >= 1 && pieceMatches.length >= 1) return true;
+
+    return false;
+}
 
 
 function onMessageHandler(target, context, msg, self) {
     if (self) return;
-    console.log(msg);
-    console.log(context.username);
-    const commandName = msg.trim();
+    const message = msg.trim();
+    const user = context.username;
+    const backseatMatches = message.match(backseatKeywords);
+    const pieceMatches = message.match(pieceNames);
+    const chessMoveMatches = message.match(chessNotation);
+    console.log(backseatMatches);
+    console.log(pieceMatches);
+    console.log(chessMoveMatches);
+    
+    if (backseating(backseatMatches, pieceMatches, chessMoveMatches))
+        timeoutUser(target, user);
 
-    if (commandName === 'timeout') {
-        client.say(target, `/timeout ${context.username} 10`);
-        console.log(`* Timing out ${context.username}`);
+    if (message === 'timeout') {
+        timeoutUser(target, user);
     }
 }
 
