8c6f82eaeffda7b66ef5350c0e94e9f6324f75f8
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-02-27 21:26:16 -0800

    Add regex patterns

--- bot.js
+++ bot.js
@@ -21,13 +21,18 @@ client.on('connected', onConnectedHandler);
 
 client.connect();
 
+const pieceNamePattern = /\bpawn\b|\bknight\b|\bbishop\b|\brook\b|\bking\b|\bqueen\b/gmi;
+const backseatKeywordsPattern = /takes|mate|missed|checkmate|defend|with/gmi;
+const chessNotationPattern = /\b[nbkqr]?x?[a-h]{1}[1-8]{1}\b/gmi;
+
+
 function onMessageHandler(target, context, msg, self) {
     if (self) return;
     console.log(msg);
     console.log(context.username);
     const commandName = msg.trim();
 
-    if (commandName == 'timeout') {
+    if (commandName === 'timeout') {
         client.say(target, `/timeout ${context.username} 10`);
         console.log(`* Timing out ${context.username}`);
     }
